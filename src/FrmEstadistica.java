8731243a215817b4fcdf0d1e245240c547d15e43
chikicraft <chikicraft726@gmail.com>
2025-02-10 09:54:19 -0500

    subida 2

--- src/FrmEstadistica.java
+++ src/FrmEstadistica.java
@@ -3,11 +3,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.print.DocFlavor.STRING;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 
@@ -15,6 +18,7 @@ public class FrmEstadistica extends JFrame{
 
     JTextField txtDato;
     JList lstMuestra;
+    JTextField txtEstadistica;
 
     public FrmEstadistica(){
 
@@ -35,9 +39,9 @@ public class FrmEstadistica extends JFrame{
         txtDato.setBounds(110, 10, 100, 25);
         getContentPane().add(txtDato);
 
-        JTextField txtDesviacion=new JTextField();
-        txtDesviacion.setBounds(270, 210, 130, 25);
-        getContentPane().add(txtDesviacion);
+        txtEstadistica=new JTextField();
+        txtEstadistica.setBounds(270, 210, 130, 25);
+        getContentPane().add(txtEstadistica);
 
         JButton btnAgregar=new JButton("Agregar");
         btnAgregar.setBounds(110, 50, 100, 25);
@@ -47,13 +51,25 @@ public class FrmEstadistica extends JFrame{
         btnQuitar.setBounds(120, 80, 90, 25);
         getContentPane().add(btnQuitar);
 
-        JButton btnDesviacion=new JButton("Desviación");
-        btnDesviacion.setBounds(140, 210, 110, 25);
-        getContentPane().add(btnDesviacion);
+        JButton btnEstadistica=new JButton("Calcular");
+        btnEstadistica.setBounds(100, 170, 110, 25);
+        getContentPane().add(btnEstadistica);
+
+        JComboBox cmbEstadistica=new JComboBox();
+        String[] opciones=new String[]{"Sumatoria", "Promedio", "Desviacion Estandar","maximo","minimo","moda"};
+        DefaultComboBoxModel mdlEstadistica=new DefaultComboBoxModel(opciones);
+        cmbEstadistica.setModel(mdlEstadistica);
+        cmbEstadistica.setBounds(80, 210, 160, 25);
+        getContentPane().add(cmbEstadistica);
 
         lstMuestra=new JList();
         lstMuestra.setBounds(285, 40, 100, 150);
         getContentPane().add(lstMuestra);
+
+        lstMuestra = new JList();
+        JScrollPane spMuestra = new JScrollPane(lstMuestra);
+        spMuestra.setBounds(285, 40, 100, 150);
+        getContentPane().add(spMuestra);
 
         btnAgregar.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent ae){
