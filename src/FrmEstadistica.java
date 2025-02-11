237728bb52967904606387eb251bf9b27026898d
chikicraft <chikicraft726@gmail.com>
2025-02-11 15:06:35 -0500

    4.2

--- src/FrmEstadistica.java
+++ src/FrmEstadistica.java
@@ -11,6 +11,26 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.ListModel;
 import javax.swing.WindowConstants;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Insets;
+import javax.swing.border.Border;
+
+class RoundedBorder implements Border {
+    private int radius;
+    RoundedBorder(int radius) {
+        this.radius = radius;
+    }
+    public Insets getBorderInsets(Component c) {
+        return new Insets(this.radius+1, this.radius+1, this.radius+2, this.radius);
+    }
+    public boolean isBorderOpaque() {
+        return true;
+    }
+    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+        g.drawRoundRect(x, y, width-1, height-1, radius, radius);
+    }
+}
 
 public class FrmEstadistica extends JFrame{
 
@@ -43,14 +63,17 @@ public class FrmEstadistica extends JFrame{
         getContentPane().add(lblDatos);
 
         JButton btnAgregar=new JButton("Agregar");
+        btnAgregar.setBorder(new RoundedBorder(10));
         btnAgregar.setBounds(110, 50, 100, 25);
         getContentPane().add(btnAgregar);
 
         JButton btnQuitar=new JButton("Quitar");
-        btnQuitar.setBounds(120, 80, 90, 25);
+        btnQuitar.setBorder(new RoundedBorder(10));
+        btnQuitar.setBounds(115, 80, 90, 25);
         getContentPane().add(btnQuitar);
         
         JButton btnEstadistica=new JButton("Calcular");
+        btnEstadistica.setBorder(new RoundedBorder(10));
         btnEstadistica.setBounds(100, 170, 110, 25);
         getContentPane().add(btnEstadistica);
 
